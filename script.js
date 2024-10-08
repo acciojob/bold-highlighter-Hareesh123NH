c6fd7b2dde3e7e2cd6f96628384bf6e6edfca880
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-10-08 19:03:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+
 function highlight() {
-    //Write your code here
-
-
+    //Write your code her
+	const bolds=document.querySelectorAll("strong");
+	// console.log(bolds);
+	bolds.forEach((b)=>{
+		// console.log(b);
+		b.classList.add("color");
+	});
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	const bolds=document.querySelectorAll("strong");
+	// console.log(bolds);
+	bolds.forEach((b)=>{
+		// console.log(b);
+		b.classList.remove("color");
+	});
 }
